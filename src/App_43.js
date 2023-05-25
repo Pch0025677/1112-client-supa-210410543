f9b13a995b6c9e1e2b1f52a7d7b4871547505bb7
pch0025677 <zzz3232329@gmail.com>
2023-05-26 02:09:05 +0800

    W15-P3: fetch menu data from local Node PostgreSQL

--- src/App_43.js
+++ src/App_43.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage_43";
 import MenuStaticPage_43 from "./pages/local/MenuStaticPage_43";
 import SupaMenuPage_43 from "./pages/supabase/MenuPage_43";
 import SupaMenuByCategoryPage_43 from "./pages/supabase/MenuByCategoryPage_43";
+import MenuNodePage_43 from "./pages/node/MenuNodePage_43";
 
 const App_43 = () => {
   
@@ -13,6 +14,7 @@ const App_43 = () => {
       <Route path='/static_43' element={<MenuStaticPage_43 />} />
       <Route path='/supa_menu_43' element={<SupaMenuPage_43 />} />
       <Route path='/supa_menu_43/:category' element={<SupaMenuByCategoryPage_43 />} />
+      <Route path='/node_menu_43' element={<MenuNodePage_43 />} />
     </Routes>
   </BrowserRouter>
   );
---
+++ src/pages/node/MenuNodePage_43.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import MenuProducts_43 from "../../components/MenuProducts_43";
+
+const base_url = `http://localhost:5000/api/node_menu_xx`
+
+let url = `${base_url}`;
+
+const MenuNodePage_43 = () => {
+  const [products, setProducts] = useState([]);
+
+  const changeFilter = (filter) => {
+    if (filter === 'all') {
+      url = `${base_url}`;
+    }else {
+      url = `${base_url}&category=eq.${filter}`;
+    }
+    getMenuData_43(filter);
+}
+
+  const getMenuData_43 = async (filter = 'all') => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(`${filter} data`, data);
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    getMenuData_43();
+  }, []);
+
+  return (
+    <section>
+      <div className="btn-container"></div>
+      <div className="section-center">
+        <section className="menu">
+          <div className="title">
+            <h2>Menu from Node Server</h2>
+            <h3>Kay_Yang, 210410543</h3>
+            <div className="underline"></div>
+          </div>
+          <div className="btn-container">
+                        <button type="button" className="filter-btn" data-id="all" onClick={() => { changeFilter(); }}>all</button><button type="button" className="filter-btn"
+                            data-id="breakfast" onClick={() => { changeFilter('breakfast'); }}>
+                            breakfast</button><button type="button" className="filter-btn" data-id="lunch" onClick={() => { changeFilter('lunch'); }}>
+                            lunch</button><button type="button" className="filter-btn" data-id="dessert" onClick={() => { changeFilter('dessert'); }}>
+                            dessert</button><button type="button" className="filter-btn" data-id="shakes" onClick={() => { changeFilter('shakes'); }}>
+                            shakes
+                        </button>
+                    </div>
+                    <MenuProducts_43 products={products}/>
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default MenuNodePage_43;
